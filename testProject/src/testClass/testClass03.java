d47b5940b2d548734f347570bad72c076fb80487
he zhifu <hezhifu1983@gmail.com>
2020-04-10 11:23:50 +0900

    add testClass03.java

---
+++ testProject/src/testClass/testClass03.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testClass;
+
+/**
+ * @author hezhifu
+ *
+ */
+public class testClass03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("test03");
+
+	}
+
+}

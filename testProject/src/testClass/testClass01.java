6dc1797e55c677acbf81a113390696c3edec6b0e
he zhifu <hezhifu1983@gmail.com>
2020-04-10 10:42:45 +0900

    add testProject

---
+++ testProject/src/testClass/testClass01.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testClass;
+
+/**
+ * @author hezhifu
+ *
+ */
+public class testClass01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("test01");
+
+	}
+
+}
---
+++ testProject/src/testClass/testClass02.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package testClass;
+
+/**
+ * @author hezhifu
+ *
+ */
+public class testClass02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("master02");
+		System.out.println("branch0409");
+		
+		System.out.println("before tag V1.0");
+
+	}
+
+}
